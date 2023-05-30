2278361d4e114798aa499c21a60b5dec63d73e31
Marvin Kweyu <mkweyu1@gmail.com>
2023-05-30 11:31:09 +0300

    chore: create studentInterface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from './student.service';
+import { Student } from '../interfaces/student.interface';
 
 
 @Component({
@@ -10,12 +11,20 @@ import { StudentService } from './student.service';
 export class HomeComponent implements OnInit {
 
   pageTitle = 'Yearbook 2023';
-  students: any = [];
+  students: [Student] = [
+    {
+      id: 1,
+      name: 'John Doe',
+      linkedin: 'https://www.linkedin.com/in/john-doe/',
+      image: 'https://via.placeholder.com/150',
+      caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  ];
 
   constructor(private studentService: StudentService) { }
 
   ngOnInit(): void {
-    this.studentService.getStudents().subscribe(student => {
+    this.studentService.getStudents().subscribe((student: [Student]) => {
       this.students = student;
     })
   }
--- src/app/home/student.service.ts
+++ src/app/home/student.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Student } from '../interfaces/student.interface';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -10,7 +12,7 @@ export class StudentService {
 
   constructor(private http: HttpClient) { }
 
-  getStudents() {
+  getStudents(): any {
     return this.http.get('https://5f5b1a88044570001674c684.mockapi.io/posts')
   }
 }
